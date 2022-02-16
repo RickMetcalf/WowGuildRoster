a3dacaec1e1d838446cdce57d9dcb7c1601f6905
Rick C Metcalf <rick.c.metcalf@gmail.com>
2022-02-16 13:26:57 -0500

    Fixed Foreign Keys

--- DataClasses/GuildDatabase.cs
+++ DataClasses/GuildDatabase.cs
@@ -15,9 +15,10 @@ namespace DataClasses
     {
         private static IConfigurationRoot _configuration;
        
-        public DbSet <DataModels.Player> Players { get; set; } 
-        public DbSet<DataModels.WowClass> Classes { get; set; }
+        public DbSet <DataModels.Player> Players { get; set; }
         public DbSet<DataModels.Spec> Specs { get; set; }
+        public DbSet<DataModels.WowClass> Classes { get; set; }
+        
         public DbSet<DataModels.Role> Roles { get; set; }
         public DbSet<DataModels.GuildRank> GuildRanks { get; set;}
         public DbSet<DataModels.Team> Teams { get; set; }  
@@ -50,71 +51,71 @@ namespace DataClasses
         {
             modelBuilder.Entity<Player>(x =>
             {
-                x.HasData(new Player() { Id = 1, PlayerName = "Heterohexual", PlayerClassID = 10, SpecializationID = 29, RoleID = 3, AltRoleID = 4, GuildRankID = 1 });
+                x.HasData(new Player() { Id = 1, PlayerName = "Heterohexual", WowClassID = 10, TeamID = 1, GuildRankID = 1 });
             });
             modelBuilder.Entity<WowClass>(a =>
             {
-                a.HasData(new WowClass() { ClassID = 1, ClassName = "Death Knight"},
-                          new WowClass() { ClassID = 2, ClassName = "Demon Hunter" },
-                          new WowClass() { ClassID = 3, ClassName = "Druid" },
-                          new WowClass() { ClassID = 4, ClassName = "Hunter" },
-                          new WowClass() { ClassID = 5, ClassName = "Mage" },
-                          new WowClass() { ClassID = 6, ClassName = "Monk" },
-                          new WowClass() { ClassID = 7, ClassName = "Paladin" },
-                          new WowClass() { ClassID = 8, ClassName = "Priest" },
-                          new WowClass() { ClassID = 9, ClassName = "Rogue" },
-                          new WowClass() { ClassID = 10, ClassName = "Shaman" },
-                          new WowClass() { ClassID = 11, ClassName = "Warlock" },
-                          new WowClass() { ClassID = 112, ClassName = "Warrior" }
+                a.HasData(new WowClass() { Id = 1, ClassName = "Death Knight" },
+                          new WowClass() { Id = 2, ClassName = "Demon Hunter" },
+                          new WowClass() { Id = 3, ClassName = "Druid" },
+                          new WowClass() { Id = 4, ClassName = "Hunter" },
+                          new WowClass() { Id = 5, ClassName = "Mage" },
+                          new WowClass() { Id = 6, ClassName = "Monk" },
+                          new WowClass() { Id = 7, ClassName = "Paladin" },
+                          new WowClass() { Id = 8, ClassName = "Priest" },
+                          new WowClass() { Id = 9, ClassName = "Rogue" },
+                          new WowClass() { Id = 10, ClassName = "Shaman" },
+                          new WowClass() { Id = 11, ClassName = "Warlock" },
+                          new WowClass() { Id = 112, ClassName = "Warrior" }
                 );
             });
             modelBuilder.Entity<Role>(b =>
             {
-                b.HasData(new Role() { RoleID = 1, RoleName = "Tank" },
-                          new Role() { RoleID = 2, RoleName = "Healer" },
-                          new Role() { RoleID = 3, RoleName = "Melee DPS" },
-                          new Role() { RoleID = 4, RoleName = "Ranged DPS" }
+                b.HasData(new Role() { Id = 1, RoleName = "Tank" },
+                          new Role() { Id = 2, RoleName = "Healer" },
+                          new Role() { Id = 3, RoleName = "Melee DPS" },
+                          new Role() { Id = 4, RoleName = "Ranged DPS" }
 
                 );
             });
             modelBuilder.Entity<Spec>(c =>
             {
-                c.HasData(new Spec() { SpecID = 1, SpecName = "Blood", ClassID = 1, RoleID = 1 },
-                          new Spec() { SpecID = 2, SpecName = "Frost", ClassID = 1, RoleID = 3 },
-                          new Spec() { SpecID = 3, SpecName = "Blood", ClassID = 1, RoleID = 3 },
-                          new Spec() { SpecID = 4, SpecName = "Vengeance", ClassID = 2, RoleID = 1 },
-                          new Spec() { SpecID = 5, SpecName = "Havoc", ClassID = 2, RoleID = 3 },
-                          new Spec() { SpecID = 6, SpecName = "Guardian", ClassID = 3, RoleID = 1 },
-                          new Spec() { SpecID = 7, SpecName = "Restoration", ClassID = 3, RoleID = 2 },
-                          new Spec() { SpecID = 8, SpecName = "Feral", ClassID = 3, RoleID = 3 },
-                          new Spec() { SpecID = 9, SpecName = "Balance", ClassID = 3, RoleID = 4 },
-                          new Spec() { SpecID = 10, SpecName = "Survival", ClassID = 4, RoleID = 3 },
-                          new Spec() { SpecID = 11, SpecName = "Beast Mastery", ClassID = 4, RoleID = 4 },
-                          new Spec() { SpecID = 12, SpecName = "Marksmanship", ClassID = 4, RoleID = 4 },
-                          new Spec() { SpecID = 13, SpecName = "Fire", ClassID = 5, RoleID = 4 },
-                          new Spec() { SpecID = 14, SpecName = "Frost", ClassID = 5, RoleID = 4 },
-                          new Spec() { SpecID = 15, SpecName = "Arcane", ClassID = 5, RoleID = 4 },
-                          new Spec() { SpecID = 16, SpecName = "Brewmaster", ClassID = 6, RoleID = 1 },
-                          new Spec() { SpecID = 17, SpecName = "Mistweaver", ClassID = 6, RoleID = 2 },
-                          new Spec() { SpecID = 18, SpecName = "Windwalker", ClassID = 6, RoleID = 3 },
-                          new Spec() { SpecID = 19, SpecName = "Protection", ClassID = 7, RoleID = 1 },
-                          new Spec() { SpecID = 20, SpecName = "Holy", ClassID = 7, RoleID = 2 },
-                          new Spec() { SpecID = 21, SpecName = "Retribution", ClassID = 7, RoleID = 3 },
-                          new Spec() { SpecID = 22, SpecName = "Discipline", ClassID = 8, RoleID = 2 },
-                          new Spec() { SpecID = 23, SpecName = "Holy", ClassID = 8, RoleID = 2 },
-                          new Spec() { SpecID = 24, SpecName = "Shadow", ClassID = 8, RoleID = 4 },
-                          new Spec() { SpecID = 25, SpecName = "Outlaw", ClassID = 9, RoleID = 3 },
-                          new Spec() { SpecID = 26, SpecName = "Subtlety", ClassID = 9, RoleID = 3 },
-                          new Spec() { SpecID = 27, SpecName = "Assassination", ClassID = 9, RoleID = 3 },
-                          new Spec() { SpecID = 28, SpecName = "Restoration", ClassID = 10, RoleID = 2 },
-                          new Spec() { SpecID = 29, SpecName = "Enhancement", ClassID = 10, RoleID = 3 },
-                          new Spec() { SpecID = 30, SpecName = "Elemental", ClassID = 10, RoleID = 4 },
-                          new Spec() { SpecID = 31, SpecName = "Destruction", ClassID = 11, RoleID = 4 },
-                          new Spec() { SpecID = 32, SpecName = "Demonology", ClassID = 11, RoleID = 4 },
-                          new Spec() { SpecID = 33, SpecName = "Affliction", ClassID = 11, RoleID = 4 },
-                          new Spec() { SpecID = 34, SpecName = "Protection", ClassID = 12, RoleID = 1 },
-                          new Spec() { SpecID = 35, SpecName = "Fury", ClassID = 12, RoleID = 3 },
-                          new Spec() { SpecID = 36, SpecName = "Arms", ClassID = 12, RoleID = 3 }
+                c.HasData(new Spec() { Id = 1, SpecName = "Blood", WowClassId = 1, RoleId = 1 },
+                          new Spec() { Id = 2, SpecName = "Frost", WowClassId = 1, RoleId = 3 },
+                          new Spec() { Id = 3, SpecName = "Blood", WowClassId = 1, RoleId = 3 },
+                          new Spec() { Id = 4, SpecName = "Vengeance", WowClassId = 2, RoleId = 1 },
+                          new Spec() { Id = 5, SpecName = "Havoc", WowClassId = 2, RoleId = 3 },
+                          new Spec() { Id = 6, SpecName = "Guardian", WowClassId = 3, RoleId = 1 },
+                          new Spec() { Id = 7, SpecName = "Restoration", WowClassId = 3, RoleId = 2 },
+                          new Spec() { Id = 8, SpecName = "Feral", WowClassId = 3, RoleId = 3 },
+                          new Spec() { Id = 9, SpecName = "Balance", WowClassId = 3, RoleId = 4 },
+                          new Spec() { Id = 10, SpecName = "Survival", WowClassId = 4, RoleId = 3 },
+                          new Spec() { Id = 11, SpecName = "Beast Mastery", WowClassId = 4, RoleId = 4 },
+                          new Spec() { Id = 12, SpecName = "Marksmanship", WowClassId = 4, RoleId = 4 },
+                          new Spec() { Id = 13, SpecName = "Fire", WowClassId = 5, RoleId = 4 },
+                          new Spec() { Id = 14, SpecName = "Frost", WowClassId = 5, RoleId = 4 },
+                          new Spec() { Id = 15, SpecName = "Arcane", WowClassId = 5, RoleId = 4 },
+                          new Spec() { Id = 16, SpecName = "Brewmaster", WowClassId = 6, RoleId = 1 },
+                          new Spec() { Id = 17, SpecName = "Mistweaver", WowClassId = 6, RoleId = 2 },
+                          new Spec() { Id = 18, SpecName = "Windwalker", WowClassId = 6, RoleId = 3 },
+                          new Spec() { Id = 19, SpecName = "Protection", WowClassId = 7, RoleId = 1 },
+                          new Spec() { Id = 20, SpecName = "Holy", WowClassId = 7, RoleId = 2 },
+                          new Spec() { Id = 21, SpecName = "Retribution", WowClassId = 7, RoleId = 3 },
+                          new Spec() { Id = 22, SpecName = "Discipline", WowClassId = 8, RoleId = 2 },
+                          new Spec() { Id = 23, SpecName = "Holy", WowClassId = 8, RoleId = 2 },
+                          new Spec() { Id = 24, SpecName = "Shadow", WowClassId = 8, RoleId = 4 },
+                          new Spec() { Id = 25, SpecName = "Outlaw", WowClassId = 9, RoleId = 3 },
+                          new Spec() { Id = 26, SpecName = "Subtlety", WowClassId = 9, RoleId = 3 },
+                          new Spec() { Id = 27, SpecName = "Assassination", WowClassId = 9, RoleId = 3 },
+                          new Spec() { Id = 28, SpecName = "Restoration", WowClassId = 10, RoleId = 2 },
+                          new Spec() { Id = 29, SpecName = "Enhancement", WowClassId = 10, RoleId = 3 },
+                          new Spec() { Id = 30, SpecName = "Elemental", WowClassId = 10, RoleId = 4 },
+                          new Spec() { Id = 31, SpecName = "Destruction", WowClassId = 11, RoleId = 4 },
+                          new Spec() { Id = 32, SpecName = "Demonology", WowClassId = 11, RoleId = 4 },
+                          new Spec() { Id = 33, SpecName = "Affliction", WowClassId = 11, RoleId = 4 },
+                          new Spec() { Id = 34, SpecName = "Protection", WowClassId = 12, RoleId = 1 },
+                          new Spec() { Id = 35, SpecName = "Fury", WowClassId = 12, RoleId = 3 },
+                          new Spec() { Id = 36, SpecName = "Arms", WowClassId = 12, RoleId = 3 }
                     );
             });
             modelBuilder.Entity<GuildRank>(b =>
@@ -123,6 +124,14 @@ namespace DataClasses
                           new GuildRank() { Id = 2, GuildRankName = "Officer" },
                           new GuildRank() { Id = 3, GuildRankName = "Member" },
                           new GuildRank() { Id = 4, GuildRankName = "Trial" }
+
+                );
+            });
+            modelBuilder.Entity<Team>(b =>
+            {
+                b.HasData(new Team() { Id = 1, TeamName = "Mythic Raiding" },
+                          new Team() { Id = 2, TeamName = "M+ Team One" }
+
 
                 );
             });
---
+++ DataClasses/Migrations/20220216181711_initial.cs
@@ -0,0 +1,171 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLibrary.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GuildRanks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GuildRankName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GuildRanks", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teams",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TeamName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teams", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlayerName = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: false),
+                    PlayerClassID = table.Column<int>(type: "int", nullable: false),
+                    TeamID = table.Column<int>(type: "int", nullable: false),
+                    GuildRankID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Players_GuildRanks_GuildRankID",
+                        column: x => x.GuildRankID,
+                        principalTable: "GuildRanks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Players_Teams_TeamID",
+                        column: x => x.TeamID,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Classes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClassName = table.Column<string>(type: "nvarchar(16)", maxLength: 16, nullable: false),
+                    PlayerId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Classes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Classes_Players_PlayerId",
+                        column: x => x.PlayerId,
+                        principalTable: "Players",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Specs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SpecName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    WowClassId = table.Column<int>(type: "int", nullable: false),
+                    RoleId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Specs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Specs_Classes_WowClassId",
+                        column: x => x.WowClassId,
+                        principalTable: "Classes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Specs_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Classes_PlayerId",
+                table: "Classes",
+                column: "PlayerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_GuildRankID",
+                table: "Players",
+                column: "GuildRankID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_TeamID",
+                table: "Players",
+                column: "TeamID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Specs_RoleId",
+                table: "Specs",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Specs_WowClassId",
+                table: "Specs",
+                column: "WowClassId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Specs");
+
+            migrationBuilder.DropTable(
+                name: "Classes");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+
+            migrationBuilder.DropTable(
+                name: "Players");
+
+            migrationBuilder.DropTable(
+                name: "GuildRanks");
+
+            migrationBuilder.DropTable(
+                name: "Teams");
+        }
+    }
+}
